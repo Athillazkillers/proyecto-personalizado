794f91289d741219f16182d35acb9c64ce743f9b
Adrian Corredor <adriandavidcc@gmail.com>
2025-05-27 01:00:41 -0500

    ola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'proyecto-personalizado';
+  isDarkTheme = false;
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+    document.documentElement.setAttribute(
+      'data-theme', 
+      this.isDarkTheme ? 'dark' : 'light'
+    );
+  }
 }
